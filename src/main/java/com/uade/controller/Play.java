7582f1a4d9870c3c442501253d378b93ca978477
sebabevitore <sbevitore@uade.edu.ar>
2026-08-24 16:46:54 -0300

    nombres

--- src/main/java/com/uade/controller/Play.java
+++ src/main/java/com/uade/controller/Play.java
@@ -3,17 +3,41 @@ package com.uade.controller;
 import com.uade.model.Personaje;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 
 public class Play {
-    ArrayList<Personaje> personajes;
-    Map<Integer, Personaje> personajesMap;
+    List<Personaje> personajes;
 
-    public Play(ArrayList<Personaje> personajes) {
-
+    public Play() {
+        personajes = new ArrayList<>();
     }
 
+    public void inicializador (){
+        List<String> nombresFemeninos = List.of(
+                "Ana", "Diana", "Elena", "Gabriela", "Ines",
+                "Karina", "Maria", "Olivia", "Quinta", "Sofia",
+                "Ursula", "Valeria"
+        );
+
+        List<String> nombresMasculinos = List.of(
+                "Bruno", "Carlos", "Fernando", "Hugo", "Javier",
+                "Luis", "Nicolas", "Pablo", "Raul", "Tomas",
+                "Walter"
+        );
+
+        for (String nombre : nombresFemeninos) {
+            personajes.add(new Personaje(nombre, false));
+        }
+
+        for (String nombre : nombresMasculinos) {
+            personajes.add(new Personaje(nombre, true));
+        }
 
 
+
+
+
+
+    }
 
 }
--- src/main/java/com/uade/model/Personaje.java
+++ src/main/java/com/uade/model/Personaje.java
@@ -3,14 +3,15 @@ package com.uade.model;
 public class Personaje {
     private int id;
     private String nombre;
-    private String apellido;
     private Color camiseta;
     private Color pelo;
     private boolean esHombre;
     private boolean usaLentes;
     private boolean tieneBarba;
+    private boolean esPelado;
 
-
-
-
+    public Personaje(String nombre, boolean esHombre) {
+        this.nombre = nombre;
+        this.esHombre = esHombre;
+    }
 }
